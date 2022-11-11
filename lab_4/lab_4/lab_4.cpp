d44d061299c551170dd7875c1158a431878dc325
Вова Гнатюк <vololdymyr.gn@gmail.com>
2022-11-11 16:11:34 +0200

    lab_4

---
+++ lab_4/lab_4/lab_4.cpp
@@ -0,0 +1,24 @@
+﻿//Заповнення масиву десятьма словами і виведення їх в зворотньому порядку
+
+#include <windows.h>
+#include <string>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	string words[10];
+
+	for (int i = 0; i < 10; i++)
+	{
+		cin >> words[i];
+	}
+
+	for (int i = 9; i >= 0; i--)
+	{
+		cout << words[i] << " ";
+	}
+}
